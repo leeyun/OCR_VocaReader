2987578ab6390871e7ddc122d6033a54776c3220
leeyun <leeyunyvn@gmail.com>
2023-11-30 14:55:50 +0900

    2023-11-30 데이터 전처리

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import pytesseract
-from imutils.perspective import four_point_transform
-import matplotlib.pyplot as plt
+import pytesseract                              
+from imutils.perspective import four_point_transform   
+import matplotlib.pyplot as plt                     
 import imutils
 import cv2
 import re
@@ -8,7 +8,41 @@ import requests
 import numpy as np
 from PIL import Image
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-a = Image.open('img.png')
-result = pytesseract.image_to_string(a, lang = 'eng')
-print(result)
+class app:
+    def __init__(self, url):
+        self.originImg = cv2.imread(url)
+
+    def findContours(self):
+        cv2.imshow("original image", self.originImg)
+        cv2.waitKey()
+
+        image = self.originImg.copy()
+        image = imutils.resize(image, width = 500)
+
+        cv2.imshow("resize image", image)
+        cv2.waitKey()
+
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(gray, (5, 5, ), 0)
+        edged = cv2.Canny(blurred, 75, 200)
+
+        cv2.imshow("gray", gray)
+        cv2.waitKey()
+        cv2.imshow("blurred", blurred)
+        cv2.waitKey()
+        cv2.imshow("edged", edged)
+        cv2.waitKey()
+
+    def grouping(self):
+    
+    def detection(self):
+    
+    def recognition(self):
+    
+    def mergeResize(self):
+    
+
+        
+url = 'img.jpg'
+test = app(url)
+test.findContours()
